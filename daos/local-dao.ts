4c707a88fcdfc058ac5e974f6d293e589fd08250
Adam Ranieri <adam.ranieri@revature.com>
2022-02-01 11:25:05 -0500

    added local dao create

---
+++ daos/local-dao.ts
@@ -0,0 +1,27 @@
+import EventDAO from "./event-dao";
+import Event from "../entities/event";
+import {writeFile, readFile} from 'fs/promises'
+import { v4 } from "uuid";
+
+export default class EventLocalDao implements EventDAO{
+
+    getAllEvents(): Promise<Event[]> {
+        throw new Error("Method not implemented.");
+    }
+    getEventById(id: string): Promise<Event> {
+        throw new Error("Method not implemented.");
+    }
+    async createEvent(event: Event): Promise<Event> {
+        event.id = v4();
+        const buffer = await readFile('events.json')
+        const events:Event[] = JSON.parse(buffer.toString())
+        events.push(event);
+        await writeFile("events.json", JSON.stringify(events))
+        return event
+        
+    }
+    updateEvent(event: Event): Promise<Event> {
+        throw new Error("Method not implemented.");
+    }
+    
+}
---
+++ specs/event-dao.spec.ts
@@ -0,0 +1,22 @@
+import EventDAO from '../daos/event-dao';
+import EventLocalDao from '../daos/local-dao';
+import Event from '../entities/event'
+
+describe("Event DAO specs", ()=>{
+
+    const eventDao:EventDAO = new EventLocalDao()
+
+    test("Should create an event", async ()=>{
+        const event:Event = {
+            id:"", 
+            location:"rooftop bar", 
+            startTime:0, endTime:0, 
+            title:"Test Event", 
+            desc:"Event for testing", 
+            status:"On Schedule"};
+
+        const savedEvent = await eventDao.createEvent(event);
+        expect(savedEvent.id).toBeTruthy()
+    })
+
+})
