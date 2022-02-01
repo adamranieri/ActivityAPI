56e952d4b1db03142daa15f5d373d790bfc7e1f6
Adam Ranieri <adam.ranieri@revature.com>
2022-02-01 18:01:19 -0500

    made mvp renamed to activities

--- middleware/request-logger.ts
+++
@@ -1,9 +0,0 @@
-import winston from 'winston';
-import expressWinston from 'express-winston';
-
-const requestLogger = expressWinston.logger({
-    transports: [new winston.transports.Console(), new winston.transports.File({filename:'./logs/eventlog.log'})],
-    expressFormat:true
-});
-
-export default requestLogger;
